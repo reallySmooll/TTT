b3bf62665632fea97dcc9f0eb6ccdd49c4e3d7ed
Jakub Skowron <jakubskowron676@gmail.com>
2023-03-06 20:44:38 +0100

    Misc changes

--- include/TTT/TTT.hpp
+++ include/TTT/TTT.hpp
@@ -1,3 +1,7 @@
+/** \file TTT.hpp
+ * \brief This is the header file and where everything is declared.
+ */
+
 #ifndef TTT_HPP
 #define TTT_HPP
 
@@ -7,39 +11,33 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 
+/** \brief The TTT namespace, which includes everything in the library. */
 namespace TTT
 {
-    typedef std::string module;
-    typedef std::string level;
-    typedef std::string color;
+    typedef std::string module; ///< Type definition for different modules.
+    typedef std::string level;  ///< Type definition for different levels.
+    typedef std::string color;  ///< Type definition for different colors.
 
-    // Main class that initializes everything.
+    /** \brief The "Window" class initializes the window and everything else (like the default event loop).
+     *
+     * The class is initialized using a "Window" constructor that takes parameters similar to the parameters of the sf::RenderWindow class constructor.
+     */
     class Window
     {
         public:
             Window(int windowWidth, int windowHeight, const sf::String& windowTitle="TTT", uint32_t windowStyle=sf::Style::Default, const sf::ContextSettings& windowContextSettings=sf::ContextSettings());
             ~Window();
 
-            sf::RenderWindow window;
+            sf::RenderWindow window;                  ///< The "window" variable is initialized with the parameters of the "Window" constructor.
 
-            static bool verbose;
-            static bool toggleFPS;
-            static bool shouldCreateDefaultEventLoop;
+            static bool verbose;                      ///< The user can choose whether to show the debug output in the terminal as well as log to a file called "TTT.log" or just log to the file.
+            static bool toggleFPS;                    ///< The user can choose whether to show the FPS at startup or require the "F3" key to be pressed to show it.
+            static bool shouldCreateDefaultEventLoop; ///< The user can choose whether to create a default immutable event loop or not.
         private:
-            void defaultEventLoop();
+            void defaultEventLoop();                  ///< Creates the default immutable event loop.
     };
 
-    // Class for button creation 'cause SFML doesn't have one I guess (weird).
-    class Button
-    {
-        public:
-            Button();
-            ~Button();
-        private:
-            sf::RectangleShape button;
-    };
-
-    // Utilities class for misc functions like the showFPS one.
+    /** \brief Utilities class for smaller and miscellaneous functions like the "showFPS" one. */
     class Utils
     {
         public:
@@ -50,31 +48,31 @@ namespace TTT
             static sf::Text createText(const sf::Font& font, unsigned int fontSize, sf::Vector2f position, std::string textContents);
     };
 
-    // Logger class not really meant to be used outside of the library.
+    /** \brief Logger class not really meant to be used outside of the library. */
     class Log
     {
         public:
             static void log(module module, level level, std::string message);
 
-            static const module TTT;
+            static const module TTT;     ///< Module used in the most critical of logs.
 
-            static const module WINDOW;
-            static const module UTILS;
-            static const module LOG;
-            static const module SFML;
+            static const module WINDOW;  ///< Module used in logging everything that has to do with the window.
+            static const module UTILS;   ///< Module used in logging everything that has to do with the utilities class.
+            static const module LOG;     ///< Module used in logging everything that has to do with the logger class.
+            static const module SFML;    ///< Module used in logging everything that has to do with the SFML library.
 
-            static const level SUCCESS;
-            static const level INFO;
-            static const level WARNING;
-            static const level ERROR;
-            static const level CRITICAL;
+            static const level SUCCESS;  ///< Success level indicates that an operation was successful.
+            static const level INFO;     ///< Info level displays basic information.
+            static const level WARNING;  ///< Warning level indicates that there's something wrong with the program but doesn't require immediate attention.
+            static const level ERROR;    ///< Error level indicates that there's something wrong with the program requires attention.
+            static const level CRITICAL; ///< Critical level indicates that there's something wrong with the program and requires immediate attention.
 
-            static const color RESET;
-            static const color RED;
-            static const color GREEN;
-            static const color WHITE;
-            static const color L_RED;
-            static const color L_YELLOW;
+            static const color RESET;    ///< Reset color resets all of the colors to the terminal default.
+            static const color RED;      ///< Red color paints output red. Used in critical errors.
+            static const color GREEN;    ///< Green color paints output green. Used when something is successful.
+            static const color WHITE;    ///< White color paints output white. Used when displaying basic information.
+            static const color L_RED;    ///< Light red color paints output light red. Used in errors.
+            static const color L_YELLOW; ///< Light yellow color paints output light yellow. used in warnings.
     };
 }
 
