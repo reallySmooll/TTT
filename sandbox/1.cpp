e7f8113b12902e616950a215bdb50ae7f3cf63ea
Jakub Skowron <jakubskowron676@gmail.com>
2023-03-07 22:32:54 +0100

    sandbox/resources/
    Moved `resources/` to `sandbox/`

--- sandbox/1.cpp
+++ sandbox/1.cpp
@@ -1,14 +1,18 @@
 #include "TTT/TTT.hpp"
 
-#define WINDOW_WIDTH  1280
-#define WINDOW_HEIGHT 720
-#define WINDOW_TITLE  "Sandbox"
+#include <string>
+
+constexpr int WINDOW_WIDTH         = 1280;
+constexpr int WINDOW_HEIGHT        = 720;
+constexpr std::string WINDOW_TITLE = "TTT";
 
 int main()
 {
     TTT::Window::verbose                      = true;
     TTT::Window::toggleFPS                    = true;
     TTT::Window::shouldCreateDefaultEventLoop = true;
+
+    TTT::Window::defaultEventLoopFontFilename = "sandbox/resources/fonts/04B_30__.TTF";
 
     TTT::Window ttt(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
